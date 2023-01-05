3af6f5c0243f27ebceee5c0e3d66c9741846b30f
Nathan Bourke <nathanbourke@Nathans-Air.home>
2023-01-05 11:17:32 -0500

    make stations appear!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import map from './map.png';
 import React from 'react';
 import './App.css';
 import Map from './Map.js';
 import axios from 'axios';
 
-const baseURL = "http://localhost:3001/stations";
+const baseUrl = "http://localhost:3001";
+const mapUrl = `${baseUrl}/map`;
 
 function App() {
-
-  const [stations, setStations] = React.useState(null);
+  const [mapNetworkData, setMapNetworkData] = React.useState(null);
 
   React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setStations(response.data);
+    axios.get(mapUrl).then((response) => {
+      setMapNetworkData(response.data);
     });
   }, []);
 
   return (
     <div className="App">
-      <Map />
+      <img style={{ "zIndex": "auto" }} className="Map-image" src={map} alt="Map of Germany" />
+      {(() => {
+        if (mapNetworkData) {
+          return <Map networkData={mapNetworkData} />;
+        }
+      })()}
     </div>
   );
 }
